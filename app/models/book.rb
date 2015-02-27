1666024dfc4bb9abf20e1d69909305a9b94ff8ae
Biotin <qayibxanov@gmail.com>
2015-02-27 19:12:23 +0100

    validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ActiveRecord::Base
+  validates :name, :description,  :price, presence: true
+  validates :image_url, format: {with: %r{\.(gif|jpg|png)\Z}i, message: ': gif, jpg, png'}
 end
